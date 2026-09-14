42e2ddc9428884dc1634e83439b29ec311e33aaf
catboylei <243333633+catboylei@users.noreply.github.com>
2026-09-14 16:41:16 +0200

    fix: make owolib a fabric dependency

--- src/main/kotlin/lei/greg/config/Config.kt
+++ src/main/kotlin/lei/greg/config/Config.kt
@@ -8,6 +8,8 @@ import java.io.FileOutputStream
 import java.nio.file.Files
 import java.util.*
 
+// todo add the ability to hide contents of text fields (for passwords)
+
 // owoconfig hates me and so does every other option
 // so this is what we are doing now <3
 
